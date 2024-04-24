747885c6e07953ab058f7a6c63ebd09ce81b5c73
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-04-24 11:10:23 +0530

    created a handling multiple events

--- Assignment: Counter App/script.js
+++ Assignment: Counter App/script.js
@@ -1,20 +1,29 @@
-let increment = document.querySelector(".inc");
-let counter = document.querySelector("h1");
-let decrement = document.querySelector(".dec");
-let reset = document.querySelector(".reset");
+function main() {
+  let increment = document.querySelector(".inc");
+  let counter = document.querySelector("h1");
+  let decrement = document.querySelector(".dec");
+  let reset = document.querySelector(".reset");
 
-function oneIncrement() {
-  counter.innerText = Number(counter.innerText) + 1;
+  let count = 0;
+
+  function oneIncrement() {
+    count = count + 1;
+    counter.innerText = count;
+  }
+
+  function oneDecrement() {
+    count = count - 1;
+    counter.innerText = count;
+  }
+
+  function resetAll() {
+    count = 0;
+    counter.innerText = count;
+  }
+
+  increment.addEventListener("click", oneIncrement);
+  decrement.addEventListener("click", oneDecrement);
+  reset.addEventListener("click", resetAll);
+
+  counter.innerText = count;
 }
-
-function oneDecrement() {
-  counter.innerText = Number(counter.innerText) - 1;
-}
-
-function resetAll() {
-  counter.innerText = 0;
-}
-
-increment.addEventListener("click", oneIncrement);
-decrement.addEventListener("click", oneDecrement);
-reset.addEventListener("click", resetAll);
